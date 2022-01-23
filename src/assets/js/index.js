0667cc772437bdb5a5fd77b5d8b9d9ddd3fbedb8
leroy leopold <leroyleopold@gmail.com>
2022-01-23 19:41:19 +0100

    #fix > add ipc

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,10 @@
 const ipc = require('electron').ipcRenderer;
 
 document.getElementById('ipc').addEventListener('click', () => {
-    ipc.send('log-error');
+
+    ipc.send('log-error', 'Fichier introuvable');
+});
+
+ipc.on('error-logged', () => {
+    alert('Une erreur a été rencontrée. Consultez le terminal pour plus de détails.');
 });
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipc = electron.ipcMain;
 
 let mainWindow;
 
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 1200, height: 600, title: "DAMP : Docker, Apache, Mysql, Php on the fly !",  icon: __dirname + '/assets/img/logo_application.png',});
-    mainWindow.removeMenu()
+    mainWindow = new BrowserWindow({width: 1200, height: 600, title: "DAMP : Docker, Apache, Mysql, Php on the fly !",  icon: __dirname + '/assets/img/logo_application.png',webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }});
+   // mainWindow.removeMenu()
     mainWindow.loadURL(`file://${__dirname}/assets/views/index.html`);
     mainWindow.on('closed', () => {
         mainWindow = null;
@@ -26,3 +30,9 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+ipc.on('log-error', (event, arg) => {
+    console.log(`Erreur (${arg}) ! Veuillez rapporter ce bug au développeur de l’application.`);
+    event.sender.send('error-logged');
+});
+
